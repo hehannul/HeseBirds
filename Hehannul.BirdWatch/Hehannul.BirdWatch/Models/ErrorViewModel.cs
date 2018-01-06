c74c3f9cf85cb03df347e537321178ea64c2c7fd
Heikki Hannula <heikki.hannula@soficta.fi>
2018-01-06 22:11:39 +0200

    Solution and Wen App project created

---
+++ Hehannul.BirdWatch/Hehannul.BirdWatch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hehannul.BirdWatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
